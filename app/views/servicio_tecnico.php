9ed60f17da1787e24bad40bd58dcfbc474edd686
francheskamedina <francheskamedina2712@gmail.com>
2026-09-10 20:50:16 -0400

    servi2

--- app/views/servicio_tecnico.php
+++ app/views/servicio_tecnico.php
@@ -1,19 +1,13 @@
-<?php
-
-require_once('assets/comunes/menu.php');
-
-?>
+<?php require_once('assets/comunes/menu.php'); ?>
 <?php require_once('assets/comunes/modal_servicio_tecnico.php'); ?>
-
 
 <main class="main m-4" id="main">
 
     <section id="hero" class="hero section m-2" style="height: auto;">
         <div>
-            <h2 class="text-center"> Administrar Servicios Técnicos</h2>
+            <h2 class="text-center fw-bold text-primary mb-4">Administrar Servicios Técnicos</h2>
 
             <div class="text-end mb-4">
-
                 <button type="button" id="btnRegistrarServicio" class="btn btn-success rounded-pill px-4" style="display:none;" data-bs-toggle="modal" data-bs-target="#modalRegistrarServicio">
                     <i class="fas fa-pen"></i>&nbsp;&nbsp; Registrar Servicio
                 </button>
@@ -21,18 +15,18 @@ require_once('assets/comunes/menu.php');
 
             <div class="table-responsive">
                 <div class="table-container">
-                    <table id="servicioTabla" class="table table-striped table-bordered text-center">
-                        <thead class="thead-dark">
+                    <table id="servicioTabla" class="table table-striped table-bordered text-center align-middle w-100">
+                        <thead class="thead-dark align-middle">
                             <tr>
                                 <th style="display: none;">ID</th>
-                                <th>Cédula</th>
-                                <th>Cliente</th> 
-                                <th>Equipo</th>
-                                <th>Falla</th>
-                                <th>Estado</th>
-                                <th>Total</th>
-                                <th>Fecha</th>
-                                <th>Acciones</th>
+                                <th class="text-center">Cédula</th>
+                                <th class="text-center">Cliente</th> 
+                                <th class="text-center">Equipo</th>
+                                <th class="text-center">Falla</th>
+                                <th class="text-center">Estado</th>
+                                <th class="text-center">Total</th>
+                                <th class="text-center">Fecha</th>
+                                <th class="text-center">Acciones</th>
                             </tr>
                         </thead>
                         <tbody></tbody>
@@ -47,13 +41,12 @@ require_once('assets/comunes/menu.php');
 
 <?php require_once('assets/comunes/footer.php'); ?>
 
-<a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i
-        class="bi bi-arrow-up-short"></i></a>
-
+<a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center">
+    <i class="bi bi-arrow-up-short"></i>
+</a>
 
 <script src="assets/js/validaciones/servicio/servicio.js"></script>
 <script src="assets/js/validaciones/servicio/servicio2.js"></script>
 
 </body>
-
 </html>
